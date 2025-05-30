73fa753150c3a94deaa892240610889510814af1
Nicole Shih <94258239+nicoleyhshih@users.noreply.github.com>
2025-05-30 16:26:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -229,7 +229,7 @@ function displayProjects() {
         <div class="content">
           <h3>${project.title}</h3>
           <p>${project.description}</p>
-          <a href="${project.link}" target="_blank">View Project</a>
+          <a href="${project.link}" target="_blank"><b>View Project</b></a>
         </div>
       `;
 
